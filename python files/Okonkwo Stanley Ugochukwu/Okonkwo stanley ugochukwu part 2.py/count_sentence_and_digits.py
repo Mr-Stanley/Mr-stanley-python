3b84686fda5593e0eff8b9927c09bb17f73a6943
Stanleys world <Stanleyugoo5@gmail.com>
2024-07-30 12:00:16 +0100

    assignment

---
+++ python files/Okonkwo Stanley Ugochukwu/Okonkwo stanley ugochukwu part 2.py/count_sentence_and_digits.py
@@ -0,0 +1,17 @@
+all_digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+ 
+all_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+string = "gunworld123"
+
+total_name = 0
+total_number = 0
+
+	if (count in all_digits):
+        	total_name += 1
+ 
+	else:
+        	total_number += 1
+
+
+ 		print(f"LETTERS {total_name} DIGITS {total_number}")
