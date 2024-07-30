ddc7249b948997b0e674e1f2d6d9167d43f88f08
Stanleys world <Stanleyugoo5@gmail.com>
2024-07-30 11:52:32 +0100

    assignment

--- python files/Okonkwo Stanley Ugochukwu/Okonkwo stanley ugochukwu part 2.py/reverse.py
+++ python files/Okonkwo Stanley Ugochukwu/Okonkwo stanley ugochukwu part 2.py/reverse.py
@@ -1,5 +1,9 @@
-def reverse(number):
+def reverse(elements):
+	reversed_number = 0
 
-	'reversed_nummber' =+ reversed_number*10 + number % 10
-	return reversed_number
+	while (elements > 0):
+		num1 = elements % 10
+		reversed_number = (reversed_number * 10) + num1
+		elements = elements // 10
+		return reversed_number
 print(reverse(894))
